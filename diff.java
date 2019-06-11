84d91e3e9d769e2483b21baf8d4a02bb6f796787
jaiswaramarjeet <amarjeetjaiswar55@gmail.com>
2019-06-11 10:35:34 -0700

    firstproject

---
+++ diff.java
@@ -0,0 +1,12 @@
+class diff
+{
+	public static void main(String args[])
+	{
+		int a,b,sum;
+		a=30;
+		b=20;
+		sum=a-b;
+		System.out.println("diff of two no.: ");
+		System.out.println(diff);
+	}
+}
