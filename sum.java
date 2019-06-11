e17b30dd6bf618d08527f8af3aea05ccf69a43b3
jaiswaramarjeet <amarjeetjaiswar55@gmail.com>
2019-06-11 10:34:37 -0700

    firstproject

---
+++ sum.java
@@ -0,0 +1,12 @@
+class sum
+{
+	public static void main(String args[])
+	{
+		int a,b,sum;
+		a=10;
+		b=20;
+		sum=a+b;
+		System.out.println("sum of two no.: ");
+		System.out.println(sum);
+	}
+}
